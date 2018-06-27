7e80d7b3441f846cff61e0fb200bcedf37535780
Nikola Trajkovic <ntrajkovic@nanointeractive.com>
2018-06-27 11:39:12 +0200

    Created recipe-mode, outputing list of recipes and display recipe details is done

--- src/app/recipes/recipe-list/recipes-list.component.ts
+++ src/app/recipes/recipe-list/recipes-list.component.ts
@@ -1,15 +1,21 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-recipes-list',
-  templateUrl: './recipes-list.component.html',
-  styleUrls: ['./recipes-list.component.css']
+    selector: 'app-recipes-list',
+    templateUrl: './recipes-list.component.html',
+    styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+    recipes: Recipe[] = [
+        new Recipe('First Recipe', 'This is simply test', 'http://images.media-allrecipes.com/userphotos/960x960/3758842.jpg'),
+        new Recipe('Second Recipe', 'This is simply test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTE613GBAI7t6A1kvMOPA8aP_3r_vSxmZ14XDdw9xN0kRePQxWY'),
+        new Recipe('Third Recipe', 'This is simply test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJbRawUzO_pSws9MjxWaC2JGvcq6R7KOTYg2a3Zw5EQlOUQlt6'),
+    ];
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
